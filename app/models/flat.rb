8b3970d1469d233092a060573c405ddfb5c81fff
Chris Geekie <chris.geekie@gmail.com>
2021-02-22 17:09:29 +0100

    Added/fixed model validation

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,8 +2,8 @@ class Flat < ApplicationRecord
   belongs_to :user
   belongs_to :bear
 
-  validates_presence_of :address, :name, :availability, :price, :capacity
-  validates_numeracality_of :capacity, only_integer: true
+  validates_presence_of :address, :name, :available, :price, :capacity
+  validates_numericality_of :capacity, only_integer: true
   validates :capacity, inclusion: 1..10
   validates :price, inclusion: 1..9000
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_153205) do
+ActiveRecord::Schema.define(version: 2021_02_22_154118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bears", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "flat_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["flat_id"], name: "index_bookings_on_flat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.boolean "available"
+    t.integer "price"
+    t.integer "capacity"
+    t.bigint "user_id", null: false
+    t.bigint "bear_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bear_id"], name: "index_flats_on_bear_id"
+    t.index ["user_id"], name: "index_flats_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +61,8 @@ ActiveRecord::Schema.define(version: 2021_02_22_153205) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "flats", "bears"
+  add_foreign_key "flats", "users"
 end
