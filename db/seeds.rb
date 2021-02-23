aaf5882d354148096088d88d9c8c95433d9cce46
Chris Geekie <chris.geekie@gmail.com>
2021-02-23 12:50:50 +0100

    Reworked seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,11 +18,11 @@ User.create(email: "amara@gmail.com", password: "password")
 User.create(email: "chris@gmail.com", password: "password")
 
 # Bear
-SPECIES = ["brown", "grizzly", "polar", "panda"]
+SPECIES = ["brown", "grizzly", "polar", "panda", "koala"]
 
 Bear.delete_all
-3.times do
-  bear = Bear.create(
+5.times do
+  Bear.create(
     species: SPECIES.sample, 
     description: Faker::Lorem.sentence(word_count: 10),
     name: Faker::Superhero.name
@@ -30,6 +30,7 @@ Bear.delete_all
 end
 
 # Flats
+bears = Bear.all.to_a
 5.times do 
   flat = Flat.new(
     name: Faker::TvShows::Simpsons.location, 
@@ -38,7 +39,7 @@ end
     price: (5..500).to_a.sample,
     capacity: (1..10).to_a.sample
   )
-  flat.bear = Bear.all.sample
+  flat.bear = bears.pop
   flat.user = User.all.sample
   flat.save
 end
