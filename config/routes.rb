4d677e2334fa6ea135ef6da44a170ca04a5bd11d
Chris Geekie <chris.geekie@gmail.com>
2021-02-23 11:43:24 +0100

    Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/about', to: 'pages#about'
+  resources :flats, only: [:index, :show] do
+    resources :bookings, only: [:create]
+  end
+  resources :bookings, only: [:index, :destroy]
 end
