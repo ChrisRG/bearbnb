777e98f2bf603bcd5ca359008ce1d8117a7e0477
Chris Geekie <chris.geekie@gmail.com>
2021-02-24 15:35:36 +0100

    Tweaked seed with descriptions for bears and flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,13 +26,21 @@ BEAR_PHOTOS = {
   koala: 'app/assets/images/koala.jpg'
 }
 
+DESCRIPTIONS = {
+  koala: "Loves eucalyptus leaves, hates bright colors. Fun!", 
+  grizzly: "Your best friend in catching salmon and eating them raw. Delicious!", 
+  panda: "Who knew sleeping 20 hours a day could be this fun? ZzZzZz!", 
+  brown: "Ha ha, who knows what this one is thinking? Be careful!", 
+  polar: "He might complain about the heat, but he sure knows his cars. Handy!"
+}
+
 5.times do
   bear_species = species.pop
   photo_path = BEAR_PHOTOS[bear_species.to_sym]
   file = File.open(photo_path)
   bear = Bear.create(
     species: bear_species.capitalize, 
-    description: Faker::Lorem.sentence(word_count: 10),
+    description: DESCRIPTIONS[bear_species.to_sym],
     name: Faker::Superhero.name
   )
   bear.photo.attach(io: file, filename: "#{bear_species}.jpg", content_type: "image/png")
@@ -49,6 +57,14 @@ FLAT_PHOTOS = [
   'app/assets/images/POLAR-BEAR-flat.jpg'
 ]
 
+FLAT_DESC = [
+  'Your home away from home! Now with a bear!',
+  'Enjoy the wonderful view! Absolutely no dogs!',
+  'A peaceful abode -- as long as the bear is sleeping!',
+  'Explore the city on foot as a real bear pursues you relentlessly!',
+  'Located in a wonderfully quiet neighborhood, since most of the neighbors have long since been eaten or moved out!'
+]
+
 bears = Bear.all.to_a
 5.times do 
   photo_path = FLAT_PHOTOS.pop
@@ -59,7 +75,7 @@ bears = Bear.all.to_a
     available: true,
     price: (5..500).to_a.sample,
     capacity: (1..10).to_a.sample,
-    description: "A great place to stay!"
+    description: FLAT_DESC.pop
   )
   flat.bear = bears.pop
   flat.user = User.all.sample
