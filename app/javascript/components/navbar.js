7b1a0100d9fe8863dadf44c14a1cbe07f41063cf
Chris Geekie <chris.geekie@gmail.com>
2021-02-25 16:40:33 +0100

    Refactored navbar and landing page banner

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,14 +1,20 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector(".navbar-lewagon");
+  const navItems = document.querySelectorAll(".nav-link")
   let pathname = window.location.pathname;
     if (navbar) {
-      if (pathname === "/" || pathname === "/about") {
+      if (pathname === "/") {
         window.addEventListener("scroll", () => {
-        console.log(pathname);
-          if (window.scrollY >= window.innerHeight) {
+          if (window.scrollY >= window.innerHeight - 50) {
             navbar.classList.add("navbar-lewagon-white");
+            navItems.forEach((item) => {
+              item.classList.remove("nav-hidden");
+            });
           } else {
             navbar.classList.remove("navbar-lewagon-white");
+            navItems.forEach((item) => {
+              item.classList.add("nav-hidden");
+            });
           }
         });
       } else {
